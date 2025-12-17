07b3bc70bb2c15d50d797edea18d17ee9a88dd2b
Dalienst Owino <oduordalienst@gmail.com>
2025-12-17 21:57:18 +0300

    add inventory linking

--- layers/models.py
+++ layers/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 from accounts.abstracts import TimeStampedModel, UniversalIdModel, ReferenceModel
+from inventory.models import Inventory
 
 
 class Layer(UniversalIdModel, TimeStampedModel, ReferenceModel):
     name = models.CharField(max_length=255, unique=True)
-    inventory = models.CharField(max_length=30)
+    inventory = models.ForeignKey(
+        Inventory, on_delete=models.CASCADE, related_name="layers"
+    )
 
     class Meta:
         verbose_name = "Layer"
