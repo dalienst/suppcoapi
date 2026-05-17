6ef531c51b11527d1927cbdefd5a8bbc3ac449a0
Dalienst <oduordalienst@gmail.com>
2026-05-17 18:57:19 +0300

    add branch mini serializers and link them inside branch serializer

--- branches/serializers.py
+++ branches/serializers.py
@@ -1,12 +1,53 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 from branches.models import Branch
 from companies.models import Company
+from products.models import Product
+
+User = get_user_model()
+
+
+class BranchProductMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "sku",
+            "reference",
+            "created_at",
+        )
+
+
+class BranchStaffMiniSerializer(serializers.ModelSerializer):
+    role_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "phone",
+            "role_name",
+        )
+
+    def get_role_name(self, obj):
+        employment = obj.employment.first() if hasattr(obj, "employment") else None
+        if employment and employment.role:
+            return employment.role.name
+        return None
 
 
 class BranchSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source="user.username", read_only=True)
     company = serializers.CharField(source="user.company.name", read_only=True)
+    head_details = BranchStaffMiniSerializer(source="head", read_only=True)
+    assigned_staff = BranchStaffMiniSerializer(many=True, read_only=True)
+    branch_products = BranchProductMiniSerializer(many=True, read_only=True)
 
     class Meta:
         model = Branch
@@ -18,6 +59,10 @@ class BranchSerializer(serializers.ModelSerializer):
             "address",
             "reference",
             "identity",
+            "head",
+            "head_details",
+            "assigned_staff",
+            "branch_products",
             "created_at",
             "updated_at",
         )
